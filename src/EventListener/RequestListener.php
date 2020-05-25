ffd2fdd793d7e43e93a6cf7d6250c525befbf867
Alexander <alexander.advanced@gmail.com>
2020-05-25 19:31:14 +0300

    updated logger

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -5,59 +5,34 @@ namespace A2Global\CRMBundle\EventListener;
 use A2Global\CRMBundle\Api\SlackApi;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\Event\ResponseEvent;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Security;
 use Throwable;
 
 class RequestListener
 {
     const SLACK_TIMEOUT = 3;
 
+    protected $error = null;
+
     protected $parameterBag;
 
-    public function __construct(ParameterBagInterface $parameterBag)
+    protected $security;
+
+    public function __construct(ParameterBagInterface $parameterBag, Security $security)
     {
         $this->parameterBag = $parameterBag;
+        $this->security = $security;
     }
 
-    public function onKernelException($exceptionEvent)
+    public function onKernelException(ExceptionEvent $exceptionEvent)
     {
-        try {
-            /** @var ExceptionEvent $exceptionEvent */
-            if (!$exceptionEvent->isMasterRequest()) {
-                return;
-            }
-
-            if ($this->parameterBag->has('a2crm.logger.slack.errors') && (bool)$this->parameterBag->get('a2crm.logger.slack.errors')) {
-                if (
-                    $this->parameterBag->has('a2crm.logger.slack.errors.ignore404')
-                    && (bool)$this->parameterBag->get('a2crm.logger.slack.errors.ignore404')
-                    && $exceptionEvent->getThrowable() instanceof NotFoundHttpException
-                ) {
-                    return;
-                }
-
-                $channel = $this->parameterBag->get('a2crm.logger.slack.channel.errors');
-
-                if (substr($channel, 0, 1) != '#') {
-                    $channel = '#' . $channel;
-                }
-                $message = sprintf(
-                    '%s%s%s (%s:%s)',
-                    $this->getRequestInfo($exceptionEvent->getRequest()),
-                    PHP_EOL,
-                    $exceptionEvent->getThrowable()->getMessage(),
-                    $exceptionEvent->getThrowable()->getFile(),
-                    $exceptionEvent->getThrowable()->getLine()
-                );
-
-                (new SlackApi($this->parameterBag->get('a2crm.logger.slack.token')))
-                    ->message($channel, $message, self::SLACK_TIMEOUT);
-            }
-        } catch (throwable $e) {
+        if (!$exceptionEvent->isMasterRequest()) {
+            return;
         }
+        $this->error = $exceptionEvent->getThrowable();
     }
 
     public function onKernelResponse(ResponseEvent $responseEvent)
@@ -67,40 +42,75 @@ class RequestListener
         }
 
         try {
-            if ($this->parameterBag->has('a2crm.logger.slack.requests') && (bool)$this->parameterBag->get('a2crm.logger.slack.requests')) {
-                if (
-                    $this->parameterBag->has('a2crm.logger.slack.requests.ignore404')
-                    && (bool)$this->parameterBag->get('a2crm.logger.slack.requests.ignore404')
-                    && $responseEvent->getResponse()->getStatusCode() == 404
-                ) {
-                    return;
-                }
-
-                $channel = $this->parameterBag->get('a2crm.logger.slack.channel.requests');
-
-                if (substr($channel, 0, 1) != '#') {
-                    $channel = '#' . $channel;
-                }
-                $message = sprintf(
-                    '%s %s %s',
-                    $responseEvent->getResponse()->getStatusCode(),
-                    $responseEvent->getRequest()->getMethod(),
-                    $responseEvent->getRequest()->getUri()
-                );
-
-                (new SlackApi($this->parameterBag->get('a2crm.logger.slack.token')))
-                    ->message($channel, $message, self::SLACK_TIMEOUT);
-            }
+            $this->sendLog($responseEvent->getRequest(), $responseEvent->getResponse());
         } catch (Throwable $exception) {
+            $a = $exception;
         }
     }
 
-    protected function getRequestInfo(Request $request)
+    protected function sendLog(Request $request, Response $response): bool
     {
-        return sprintf(
-            '%s %s',
+        if (!$this->parameterBag->has('a2crm')) {
+            return false;
+        }
+        $parameters = $this->parameterBag->get('a2crm');
+
+        foreach (['enabled', 'token', 'channels'] as $var) {
+            if (!isset($parameters['logger']['slack'][$var]) || empty($parameters['logger']['slack'][$var])) {
+                return false;
+            }
+        }
+
+        foreach ($parameters['logger']['slack']['channels'] as $channelName => $channelOptions) {
+            if (!$this->shouldSendToChannel($channelOptions ?? [], $request, $response)) {
+                continue;
+            }
+            $this->send($channelName, $request, $response);
+        }
+    }
+
+    protected function shouldSendToChannel(array $channel, Request $request, Response $response): bool
+    {
+        $excludeMethods = $channel['exclude_methods'] ?? [];
+
+        foreach ($excludeMethods as $excludeMethod) {
+            if ($request->getMethod() == strtoupper($excludeMethod)) {
+                return false;
+            }
+        }
+
+        $excludeCodes = $channel['exclude_codes'] ?? [];
+
+        foreach ($excludeCodes as $excludeCode) {
+            if ($response->getStatusCode() == $excludeCode) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    protected function send(string $channelName, Request $request, Response $response)
+    {
+        $message = sprintf(
+            '*%s* `%s:%s` %s',
+            $this->security->getUser() ? ucfirst($this->security->getUser()->getUsername()) : 'Guest',
             $request->getMethod(),
+            $response->getStatusCode(),
             $request->getUri()
         );
+
+        if ($this->error instanceof Throwable) {
+            $message .= PHP_EOL . sprintf(
+                    '%s (%s#%s)',
+                    $this->error->getMessage(),
+                    $this->error->getFile(),
+                    $this->error->getLine()
+                );
+        }
+
+        $parameters = $this->parameterBag->get('a2crm');
+
+        (new SlackApi($parameters['logger']['slack']['token']))->message($channelName, $message, self::SLACK_TIMEOUT);
     }
 }
